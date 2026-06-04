2184c8bd82b553a04a2d5e23ce44e4733d66faeb
ANNI <animeshporwal0955@gmail.com>
2026-06-04 15:49:49 +0530

    completed the data cleaning

---
+++ src/data/data-preprocessing.py
@@ -0,0 +1,58 @@
+import pandas as pd 
+from pathlib import Path
+import logging 
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(message)s"
+)
+
+logger = logging.getLogger(__name__)
+
+
+ROOT_DIR = Path(__file__).parent.parent.parent
+
+
+DATA_DIR = ROOT_DIR / "data"
+RAW_DATA_PATH = DATA_DIR / "raw" / "CO2_Emissions_Canada.csv"
+INTERIM_DATA_PATH = DATA_DIR / "interim" / "cleaned.csv"
+
+def clean_data(df: pd.DataFrame) -> pd.DataFrame:
+    logging.info('Data Cleaning started')
+
+    # solved the lettercase problem 
+    cat_cols = ['Make',	'Model', 'Vehicle Class']
+    for col in cat_cols:
+        df[col] = df[col].str.lower()
+    
+    logging.info('Converted to lowercase')
+
+    # solved the duplicacy problem 
+    df = df.drop_duplicates()
+    
+    logging.info('removed te duplicated rows')
+    return df
+
+
+def main():
+    logging.info('Loading dataset')
+    df = pd.read_csv(RAW_DATA_PATH)
+
+    logger.info('Dataset loaded successfully')
+
+    cleaned_df = clean_data(df)
+
+    INTERIM_DATA_PATH.parent.mkdir(
+        parents=True,
+        exist_ok=True
+    )
+
+    cleaned_df.to_csv(
+        INTERIM_DATA_PATH,
+        index=False
+    )
+
+    logging.info(f'Dataset saved at {INTERIM_DATA_PATH}')
+
+if __name__ == '__main__':
+    main()
