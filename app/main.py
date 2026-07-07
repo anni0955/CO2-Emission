e1b02bc75ea44d6bcb3ff5d3a7ef904326d59cd6
ANNI <animeshporwal0955@gmail.com>
2026-07-07 09:04:25 +0530

    api building is completed and it is working

--- app/main.py
+++ app/main.py
@@ -1,15 +1,20 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
 
 from app.routers.prediction import router
-
 app = FastAPI(
     title='CO2 Emission Prediction',
     version='1.0.0'
 )
+app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory='templates')
+
 
 app.include_router(router)
 
-@app.get('/')
-def home():
-    return {'message': 'CO2 Emission Prediction API'}
+@app.get('/', response_class=HTMLResponse)
+def home(request: Request):
+    return templates.TemplateResponse(request=request, name='index.html')
 
--- app/routers/prediction.py
+++ app/routers/prediction.py
@@ -1,11 +1,51 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Form, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
 
-from app.schemas import VehicleData
 from app.predictor import predict
 
 router = APIRouter()
 
-@router.post('/predict')
-def predict_co2(vehicle: VehicleData):
-    prediction = predict(vehicle.model_dump())
-    return {'predicted_co2_emission': prediction}
+templates = Jinja2Templates(directory="templates")
+
+
+@router.post("/predict", response_class=HTMLResponse)
+def predict_co2(
+    request: Request,
+
+    Make: str = Form(...),
+    Model: str = Form(...),
+    Vehicle_Class: str = Form(...),
+    Engine_Size_L: float = Form(...),
+    Cylinders: int = Form(...),
+    Transmission: str = Form(...),
+    Fuel_Type: str = Form(...),
+    Fuel_Consumption_City_L_100_km: float = Form(...),
+    Fuel_Consumption_Hwy_L_100_km: float = Form(...),
+    Fuel_Consumption_Comb_L_100_km: float = Form(...),
+    Fuel_Consumption_Comb_mpg: float = Form(...)
+):
+
+    vehicle = {
+        "Make": Make,
+        "Model": Model,
+        "Vehicle_Class": Vehicle_Class,
+        "Engine_Size_L": Engine_Size_L,
+        "Cylinders": Cylinders,
+        "Transmission": Transmission,
+        "Fuel_Type": Fuel_Type,
+        "Fuel_Consumption_City_L_100_km": Fuel_Consumption_City_L_100_km,
+        "Fuel_Consumption_Hwy_L_100_km": Fuel_Consumption_Hwy_L_100_km,
+        "Fuel_Consumption_Comb_L_100_km": Fuel_Consumption_Comb_L_100_km,
+        "Fuel_Consumption_Comb_mpg": Fuel_Consumption_Comb_mpg,
+    }
+
+    prediction = predict(vehicle)
+
+    return templates.TemplateResponse(
+    request=request,
+    name="result.html",
+    context={
+        "prediction": round(float(prediction), 2)
+    }
+)
